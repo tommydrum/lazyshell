6777d8df0771b88f38531d6cbf5821a4a4f230a4
Thomas Miller <tommy@tommydrum.net>
2018-11-13 12:02:35 -0800

    refactor for better builtin command support, add some basic functions

--- shell.py
+++ shell.py
@@ -1,11 +1,51 @@
 import os
 import sys
+import io
 
+# Module functions
+
+# no-op
+nop = lambda *a, **k: None
+
+#directory nav
+def curdir(args):
+    print(os.getcwd())
+
+def chdir(args):
+    try:
+        os.chdir(args[0])
+    except OSError:
+        print("Failed")
+
+def listdir(args):
+    for i in os.listdir(os.getcwd()):
+        print(i)
+        #todo: differentiate directories from files
+
+# init builtin functions
+def initbuiltin():
+    functions = {
+        "exit": exit,
+        "dir": curdir,
+        "cwd": curdir,
+        "ls": listdir,
+        "cd": chdir,
+        '': nop
+    }
+    return functions
+
+# init
+bi = initbuiltin()
+
+
+# debug flag
 if sys.argv.__contains__("-v"):
     debug = True
     print("Verbosity is enabled.")
 else:
     debug = False
+
+# program loop
 while True:
     # prompt
     sys.stdout.write("$ ")
@@ -19,17 +59,24 @@ while True:
     if debug is True:
         sys.stdout.write("splitin is: ")
         print(splitin)
+        sys.stdout.write("args is: ")
+        print(splitin[1:])
 
     # builtin commands (including no command)
-    if splitin[0] == "exit":
-        exit()
-    if splitin[0] == '':
+    try:
+        bi[splitin[0]](splitin[1:])
         continue
+    except KeyError:
+        nop()
 
     # Process a real command
     id = os.fork()
     if id == 0:
-        os.execvp(splitin[0], splitin)
+        try:
+            os.execvp(splitin[0], splitin)
+        except OSError:
+            print("Command not found.")
+            exit()
     else:
         os.waitpid(id, 0)
 
